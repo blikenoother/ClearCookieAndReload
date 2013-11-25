8fa29050c7949c158eb2d44fc0efe6fae40ff9dd
chirag <chirag@viralmint.com>
2013-11-25 11:17:01 +0530

    all files added

---
+++ background.js
@@ -0,0 +1,32 @@
+// triggered when user clicks on installed extention icon
+chrome.browserAction.onClicked.addListener(function(tab) {
+    
+    // get detail of currect active tab
+    chrome.tabs.query({active:true}, function(tabs){
+        
+        // retrive domain from active tab
+        var url = tabs[0].url;
+        var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+        var d = matches && matches[1].replace('www.','');
+        d = '.'+d;
+
+        // get all cookies for domain
+        chrome.cookies.getAll({domain: d}, function (cookies) {
+
+            // iterate on cookie to get cookie detail
+            for (var i=0; i<cookies.length; i++) {
+                var url = "http" + (cookies[i].secure ? "s" : "") + "://" + cookies[i].domain + cookies[i].path;
+                var cname = cookies[i].name;
+
+                // delete cookie
+                chrome.cookies.remove({
+                    "url": url,
+                    "name": cname
+                });
+            }
+
+            // reload currect active tab
+            chrome.tabs.reload();
+        });
+    });
+});
