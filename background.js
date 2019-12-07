ea2835ec4c6b5b6191b0984cd067fd743bd24a32
Mackenzie Zastrow <zastrowm+github@gmail.com>
2019-12-07 14:31:37 -0600

    Clear cookies for the current url in addition to the domain
    I found that some cookies were not being cleared for me

--- background.js
+++ background.js
@@ -1,28 +1,36 @@
 // triggered when user clicks on installed extention icon
-chrome.browserAction.onClicked.addListener(function(tab) {
-    
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+    let url = tab.url;
+
+    // clear all cookies for the current url
+    chrome.cookies.getAll({ url: url }, function (cookies) {
+        console.log("Clearing cookies for active urL", url, cookies);
+        clearCookies(cookies);
+    });
+
     // retrive domain from active tab
-    var url = tab.url;
-    var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-    var d = matches && matches[1].replace('www.','');
-    d = '.'+d;
+    let matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    let domain = matches && matches[1].replace('www.', '');
+    domain = "." + domain;
 
     // get all cookies for domain
-    chrome.cookies.getAll({domain: d}, function (cookies) {
-
-        // iterate on cookie to get cookie detail
-        for (var i=0; i<cookies.length; i++) {
-            var url = "http" + (cookies[i].secure ? "s" : "") + "://" + cookies[i].domain + cookies[i].path;
-            var cname = cookies[i].name;
-
-            // delete cookie
-            chrome.cookies.remove({
-                "url": url,
-                "name": cname
-            });
-        }
-
-        // reload currect active tab
-        chrome.tabs.reload();
+    chrome.cookies.getAll({ domain: domain }, function (cookies) {
+        console.log("Clearing cookies for domain", domain, cookies);
+        clearCookies(cookies);
     });
 });
+
+function clearCookies(cookies) {
+    // iterate on cookie to get cookie detail
+    for (let i = 0; i < cookies.length; i++) {
+        let url = "http" + (cookies[i].secure ? "s" : "") + "://" + cookies[i].domain + cookies[i].path;
+        let cname = cookies[i].name;
+
+        // delete cookie
+        chrome.cookies.remove({
+            url: url,
+            name: cname
+        });
+    }
+}
